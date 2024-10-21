2f9ae938608339eeeaa132c07d0a4962a299adad
PrabathBnk <prabathbanukada@gmail.com>
2024-10-21 23:38:16 +0530

    Report List View Implementation

--- src/main/java/edu/icet/controller/report/ReportFormController.java
+++ src/main/java/edu/icet/controller/report/ReportFormController.java
@@ -3,11 +3,13 @@ package edu.icet.controller.report;
 import com.jfoenix.controls.JFXListView;
 import edu.icet.service.ServiceFactory;
 import edu.icet.service.custom.ReportService;
-import edu.icet.service.custom.UserLogService;
 import edu.icet.service.custom.UserService;
+import edu.icet.util.PdfViewer;
 import edu.icet.util.ReportType;
 import edu.icet.util.SceneSwitcher;
 import edu.icet.util.ServiceType;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,7 +18,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class ReportFormController implements Initializable {
@@ -28,18 +32,26 @@ public class ReportFormController implements Initializable {
     private VBox sectionSalesReport;
 
     @FXML
-    private JFXListView<?> listEmployeeReport;
+    private JFXListView<String> listEmployeeReport;
 
     @FXML
-    private JFXListView<?> listInventoryReport;
+    private JFXListView<String> listInventoryReport;
 
     @FXML
-    private JFXListView<?> listSalesReport;
+    private JFXListView<String> listSalesReport;
 
     @FXML
-    private JFXListView<?> listSupplierReport;
+    private JFXListView<String> listSupplierReport;
 
     private ReportService reportService;
+    private HashMap<String,File> inventoryReports;
+    File inventoryReportDir;
+
+    private HashMap<String,File> employeeReports;
+    File employeeReportDir;
+
+    private HashMap<String,File> supplierReports;
+    File supplierReportDir;
 
 
     @FXML
@@ -68,12 +80,13 @@ public class ReportFormController implements Initializable {
     @FXML
     void btnGenerateEmployeeReport(ActionEvent event) {
         if (!reportService.generateReport(ReportType.EMPLOYEE_REPORT)) new Alert(Alert.AlertType.ERROR, "Something went wrong!").show();
+        setHashMapToListView(employeeReports, employeeReportDir, listEmployeeReport);
     }
 
     @FXML
     void btnGenerateInventoryReport(ActionEvent event) {
-
         if (!reportService.generateReport(ReportType.INVENTORY_REPORT)) new Alert(Alert.AlertType.ERROR, "Something went wrong!").show();
+        setHashMapToListView(inventoryReports, inventoryReportDir, listInventoryReport);
     }
 
     @FXML
@@ -83,8 +96,8 @@ public class ReportFormController implements Initializable {
 
     @FXML
     void btnGenerateSupplierReport(ActionEvent event) {
-
         if (!reportService.generateReport(ReportType.SUPPLIER_REPORT)) new Alert(Alert.AlertType.ERROR, "Something went wrong!").show();
+        setHashMapToListView(supplierReports, supplierReportDir, listSupplierReport);
     }
 
 
@@ -95,5 +108,30 @@ public class ReportFormController implements Initializable {
 
         UserService userService = ServiceFactory.getInstance().getService(ServiceType.USER);
         sectionSalesReport.setVisible(userService.lastLogIsAdmin());
+
+        inventoryReports = new HashMap<>();
+        inventoryReportDir = new File("src/main/resources/generated_reports/inventory_reports");
+        setHashMapToListView(inventoryReports, inventoryReportDir, listInventoryReport);
+
+        employeeReports = new HashMap<>();
+        employeeReportDir = new File("src/main/resources/generated_reports/employee_reports");
+        setHashMapToListView(employeeReports, employeeReportDir, listEmployeeReport);
+
+        supplierReports = new HashMap<>();
+        supplierReportDir = new File("src/main/resources/generated_reports/supplier_reports");
+        setHashMapToListView(supplierReports, supplierReportDir, listSupplierReport);
+    }
+
+    private void setHashMapToListView(HashMap<String,File> reportsHashMap, File reportsDirectory, JFXListView<String> listView){
+        for (File file : reportsDirectory.listFiles()) {
+            reportsHashMap.put(file.getName(), file);
+        }
+        listView.setItems(FXCollections.observableArrayList(reportsDirectory.list()));
+        listView.getSelectionModel().getSelectedItems().addListener((InvalidationListener) report -> {
+            File file = reportsHashMap.get(report.toString().replaceAll("[\\[\\]]", ""));
+            PdfViewer.getInstance().viewPdf(file.getPath());
+            report = null;
+        });
+
     }
 }
